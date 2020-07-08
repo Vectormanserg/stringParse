970dc83865fe3fda254ae506268ea117e9e0f0ec
Vectormanserg <vectormanserg@gmail.com>
2020-07-08 10:04:53 +0300

    Parser is ready

--- Program.cs
+++ Program.cs
@@ -10,11 +10,11 @@ namespace stringParse
     {
         static void Main(string[] args)
         {
-            Parse();
+            Calculate(Parse());
         }
-        static void Parse()
+        static string[] Parse()
         {
-            string func = "2-x*x";
+            string func = "2-x^2+25*7-5^2";
             char[] math = func.ToCharArray();
             string TempStr = "", Oper = "";
             char[] MathFunc = new char[5] { '+', '-', '*', '/', '^' };
@@ -54,16 +54,141 @@ namespace stringParse
                     Difs.Add(TempStr);
                 }
             }
-            foreach (string x in Difs)
+            string[] args = new string[Difs.Count];
+            for (int i = 0; i< Difs.Count; i++)
             {
-                Console.WriteLine(x);
+                args[i] = Difs[i];
             }
+            return args;
         }
-        static double Calculate()
+        static double Calculate(string[] args)
         {
+            double x = 3;
             double result;
-            result = 1.25;
+            double temp;
+            string tempstr1, tempstr2;
+            List<string> Inserted = new List<string>();
+            for (int n = 0; n<args.Length; n++)
+            {
+                if (args[n] == "x")
+                {
+                    Inserted.Add(x.ToString());
+                }
+                else Inserted.Add(args[n]);
+            }
+
+            do
+            {
+                int i;
+                if (Inserted.Contains("^"))
+                {
+                    i = Inserted.IndexOf("^");
+                    tempstr1 = Inserted[i - 1];
+                    tempstr2 = Inserted[i + 1];
+                    temp = Math.Pow(double.Parse(tempstr1), double.Parse(tempstr2));
+                    ListMod(i, Inserted, temp);
+                }
+                else if (Inserted.Contains("*") && Inserted.Contains("/"))
+                {
+                    if (Inserted.IndexOf("*") < Inserted.IndexOf("/"))
+                    {
+                        i = Inserted.IndexOf("*");
+                        tempstr1 = Inserted[i - 1];
+                        tempstr2 = Inserted[i + 1];
+                        temp = double.Parse(tempstr1) * double.Parse(tempstr2);
+                        ListMod(i, Inserted, temp);
+                    }
+                    else
+                    {
+                        i = Inserted.IndexOf("/");
+                        tempstr1 = Inserted[i - 1];
+                        tempstr2 = Inserted[i + 1];
+                        temp = double.Parse(tempstr1) / double.Parse(tempstr2);
+                        ListMod(i, Inserted, temp);
+                    }
+                }
+                else if (Inserted.Contains("*"))
+                {
+                    i = Inserted.IndexOf("*");
+                    tempstr1 = Inserted[i - 1];
+                    tempstr2 = Inserted[i + 1];
+                    temp = double.Parse(tempstr1) * double.Parse(tempstr2);
+                    ListMod(i, Inserted, temp);
+                }
+                else if (Inserted.Contains("/"))
+                {
+                    i = Inserted.IndexOf("/");
+                    tempstr1 = Inserted[i - 1];
+                    tempstr2 = Inserted[i + 1];
+                    temp = double.Parse(tempstr1) / double.Parse(tempstr2);
+                    ListMod(i, Inserted, temp);
+                }
+                else if (Inserted.Contains("+") && Inserted.Contains("-"))
+                {
+                    if (Inserted.IndexOf("+") < Inserted.IndexOf("-"))
+                    {
+                        i = Inserted.IndexOf("+");
+                        tempstr1 = Inserted[i - 1];
+                        tempstr2 = Inserted[i + 1];
+                        temp = double.Parse(tempstr1) + double.Parse(tempstr2);
+                        ListMod(i, Inserted, temp);
+                    }
+                    else
+                    {
+                        i = Inserted.IndexOf("-");
+                        tempstr1 = Inserted[i - 1];
+                        tempstr2 = Inserted[i + 1];
+                        temp = double.Parse(tempstr1) - double.Parse(tempstr2);
+                        ListMod(i, Inserted, temp);
+                    }
+                }
+                else if (Inserted.Contains("+"))
+                {
+                    i = Inserted.IndexOf("+");
+                    tempstr1 = Inserted[i - 1];
+                    tempstr2 = Inserted[i + 1];
+                    temp = double.Parse(tempstr1) + double.Parse(tempstr2);
+                    ListMod(i, Inserted, temp);
+                }
+                else if (Inserted.Contains("-"))
+                {
+                    i = Inserted.IndexOf("-");
+                    tempstr1 = Inserted[i - 1];
+                    tempstr2 = Inserted[i + 1];
+                    temp = double.Parse(tempstr1) - double.Parse(tempstr2);
+                    ListMod(i, Inserted, temp);
+                }
+            }
+            while (Inserted.Count > 1);
+            result = double.Parse(Inserted[0]);
             return result;
+        }
+        static List<string> ListMod(int i, List<string> Inserted, double temp)
+        {
+            List<string> operate = new List<string>();
+            for (int z = 0; z < Inserted.Count; z++)
+            {
+                if (z < i - 1 || z > i + 1)
+                {
+                    string s = Inserted[z];
+                    operate.Add(Inserted[z]);
+                }
+                else if (z == i - 1 || z == i + 1)
+                {
+                }
+                else
+                {
+                    string s = temp.ToString();
+                    operate.Add(temp.ToString());
+                }
+            }
+            Inserted.Clear();
+            foreach (string q in operate)
+            {
+                string s = q;
+                Inserted.Add(q);
+            }
+            return operate;
         }
     }
 }
